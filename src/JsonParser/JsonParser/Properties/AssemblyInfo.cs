2bc29b4203c0d7c6cf3b49ae1a4e8e20f311c85e
unknown <Dimebrain@.(none)>
2010-12-21 18:23:14 -0500

    Initial import

---
+++ src/JsonParser/JsonParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dimebrain JSON Parser")]
+[assembly: AssemblyDescription("A parser for the JSON specification.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dimebrain")]
+[assembly: AssemblyProduct("JsonParser")]
+[assembly: AssemblyCopyright("Public Domain")]
+[assembly: AssemblyTrademark("Public Domain")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("486c87de-5529-47da-a31f-eae00e762c84")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("JsonParser.Tests")]
